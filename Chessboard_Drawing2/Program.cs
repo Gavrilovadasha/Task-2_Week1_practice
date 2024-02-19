84fddbff5b66d7509aed439998d9ebaa40acd1a4
Gavrilovadasha <gavrilovadasha239@gmail.com>
2024-02-19 18:10:47 +0300

    1

---
+++ Chessboard_Drawing2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Chessboard
+{
+    static void Main()
+    {
+        char[,] board = new char[8, 8]
+        {
+            {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+        };
+
+        Console.WriteLine("   a b c d e f g h");
+        Console.WriteLine("  -----------------");
+        for (int i = 0; i < 8; i++)
+        {
+            Console.Write(8 - i + " ");
+            for (int j = 0; j < 8; j++)
+            {
+                Console.Write("|" + board[i, j]);
+            }
+            Console.WriteLine("|");
+            Console.WriteLine("  -----------------");
+        }
+    }
+}
---
+++ Task№2_Week1_Gavrilova/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ChessBishop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод координат ладьи и фигуры.
+            Console.WriteLine("Введите координаты слона и фигуры в формате 'a1 b2':");
+            string input = Console.ReadLine();
+            string[] coordinates = input.Split(' ');
+
+            // Проверка корректности введенных данных.
+            if (coordinates.Length != 2 || !IsValidCoordinate(coordinates[0]) || !IsValidCoordinate(coordinates[1]))
+            {
+                Console.WriteLine("Введены некорректные координаты");
+                return;
+            }
+
+            // Получение координат ладьи и фигуры.
+            char bishopColomns = coordinates[0][0];
+            int bishopRows = int.Parse(coordinates[0][1].ToString());
+            char figureColomns = coordinates[1][0];
+            int figureRows = int.Parse(coordinates[1][1].ToString());
+
+            // Проверка бьёт ли ладья фигуру.
+            if ( Math.Abs (bishopColomns - figureColomns) == Math.Abs(bishopRows - figureRows))
+            {
+                Console.WriteLine("Слон сможет побить фигуру");
+            }
+            else
+            {
+                Console.WriteLine("Слон не сможет побить фигуру");
+            }
+        }
+
+        // Проверка корректности координат.
+        static bool IsValidCoordinate(string coordinate)
+        {
+            if (coordinate.Length != 2)
+            {
+                return false;
+            }
+
+            char colomns = coordinate[0];
+            int rows = int.Parse(coordinate[1].ToString());
+
+            return (colomns >= 'a' && colomns <= 'h') && (rows >= 1 && rows <= 8);
+        }
+
+    }
+}
